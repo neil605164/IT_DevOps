b89c04cbee4b28bd860d142dcc6b4c4f91d86427
neil605164 <neil605164work@gmail.com>
2019-09-08 02:25:35 +0800

    [新增] 第7天內容

---
+++ Day7/Redis-Example/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+include 'config.sample.inc.php';
+$config['servers'] = array();
+$config['servers'][] = array(
+  'name'   => "Test_6379",
+  'host'   => "Test_6379",
+  'port'   => "6379",
+  'filter' => '*',
+);
+
+?>
